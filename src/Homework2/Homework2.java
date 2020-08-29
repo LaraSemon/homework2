b74a984bc6c240559f8e45ee23c1f3ba8429bc39
Lara <tatasemon@mail.ru>
2020-08-30 02:43:57 +0500

    пишет то, что не понимаю

--- src/Homework2/Homework2.java
+++ src/Homework2/Homework2.java
@@ -5,18 +5,30 @@ import java.util.Arrays;
 public class Homework2 {
 
     public static void main(String[] args) {
-
-        int[] sample = new  int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         int i;
-        for (i = 0; i < sample.length; i++) ;
+        int[] sample = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
 
-        {
+
+        for (i = 0; i < sample.length; i++) {
             if (sample[i] == 0) {
                 sample[i] = 1;
-            } else {
+            } else
                 sample[i] = 0;
-            }
+
         }
         System.out.println(Arrays.toString(sample));
     }
+
+
+    public static void stepByStep() {
+        int i;
+        int[] arr = new int[8];
+        String[] stringsArr = new String[8];
+
+        for (i = 0; i < arr.length; i += 3) {
+            System.out.println(Arrays.toString(new String[]{arr[i] + " "});
+            arr[i]+=3;
+        }
+    }
 }
+
